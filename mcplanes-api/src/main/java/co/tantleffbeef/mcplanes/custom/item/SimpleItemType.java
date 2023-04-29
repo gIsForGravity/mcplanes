0c748c4d3c84a375892755c79a47567934e84914
gIsForGravity <gavin.f.tantleff@gmail.com>
2023-04-29 19:14:21 -0400

    add a second constructor to SimpleItemType

--- mcplanes-api/src/main/java/co/tantleffbeef/mcplanes/custom/item/SimpleItemType.java
+++ mcplanes-api/src/main/java/co/tantleffbeef/mcplanes/custom/item/SimpleItemType.java
@@ -11,18 +11,24 @@ public class SimpleItemType implements CustomItemType {
     private final NamespacedKey id;
     private final Plugin plugin;
     private final boolean customModel;
+    private final Material baseItemMaterial;
     private final String name;
 
     public SimpleItemType(Plugin namespace, String id, boolean customModel, String name) {
+        this(namespace, id, customModel, name, Material.STICK);
+    }
+
+    public SimpleItemType(Plugin namespace, String id, boolean customModel, String name, Material baseItemMaterial) {
         this.plugin = namespace;
         this.id = new NamespacedKey(namespace, id);
         this.customModel = customModel;
         this.name = name;
+        this.baseItemMaterial = baseItemMaterial;
     }
 
     @Override
     public @NotNull Material baseMaterial() {
-        return Material.STICK;
+        return baseItemMaterial;
     }
 
     @Override
