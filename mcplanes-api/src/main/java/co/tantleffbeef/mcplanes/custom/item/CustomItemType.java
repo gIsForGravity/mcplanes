0b5c15ec9c4f4b9233f29107fd8eb88bae6e04c3
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-06-16 13:54:47 -0400

    CustomItemType.asInstanceOf !!!

--- mcplanes-api/src/main/java/co/tantleffbeef/mcplanes/custom/item/CustomItemType.java
+++ mcplanes-api/src/main/java/co/tantleffbeef/mcplanes/custom/item/CustomItemType.java
@@ -1,8 +1,12 @@
 package co.tantleffbeef.mcplanes.custom.item;
 
+import co.tantleffbeef.mcplanes.CustomNbtKey;
+import co.tantleffbeef.mcplanes.KeyManager;
+import co.tantleffbeef.mcplanes.ResourceManager;
 import co.tantleffbeef.mcplanes.pojo.serialize.CustomItemNbt;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,4 +23,26 @@ public interface CustomItemType {
      *             contains id, so that doesn't need to be set
      */
     default void addAdditionalNbtItemData(@NotNull CustomItemNbt data) {}
+
+    static <T extends CustomItemType> @Nullable T asInstanceOf(Class<T> type, ItemStack itemStack,
+                                                                      KeyManager<CustomNbtKey> keyManager, ResourceManager resourceManager) {
+        ItemMeta meta = itemStack.getItemMeta();
+        if (meta == null)
+            return null;
+
+        final var data = meta.getPersistentDataContainer();
+
+        if (!CustomItemNbt.hasCustomItemNbt(data, keyManager))
+            return null;
+
+        final var itemNbt = CustomItemNbt.fromPersistentDataContainer(data, keyManager);
+        final var itemType = resourceManager.getCustomItemType(itemNbt.id);
+
+
+        if (!(type.isInstance(itemType)))
+            return null;
+
+        return (T) itemType;
+
+    }
 }
